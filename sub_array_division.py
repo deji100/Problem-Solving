65ff00b64ab391696c6cec06af7e68498bb8eb20
Deji <adeyemideji9@gmail.com>
2023-01-30 10:05:49 +0100

    solved sub array division

---
+++ sub_array_division.py
@@ -0,0 +1,50 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'birthday' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY s
+#  2. INTEGER d
+#  3. INTEGER m
+#
+
+def birthday(s, d, m):
+    count = 0
+    counter = 0
+    
+    for i in range(len(s)-m+1):
+        if sum(s[i:m+counter]) == d:
+            count += 1
+            counter += 1
+        else:
+            counter += 1
+    else:
+        return count
+            
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    s = list(map(int, input().rstrip().split()))
+
+    first_multiple_input = input().rstrip().split()
+
+    d = int(first_multiple_input[0])
+
+    m = int(first_multiple_input[1])
+
+    result = birthday(s, d, m)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
