381156aa77232fa545c20f0b31427bcf7d7bf342
Faruq Adeyemi Deji <adeyemideji9@gmail.com>
2024-09-06 09:18:35 +0100

    completed print function and combinations with replacements tool

---
+++ v2/combination-with-replacements.py
@@ -0,0 +1,15 @@
+from itertools import combinations_with_replacement
+
+
+if __name__ == "__main__":
+    n = input()
+    
+    elements = n[:len(n) - 2]
+    num = int(n[len(elements)+1:])
+        
+    sorted_elements = "".join(sorted(elements))
+
+    combined_sorted_list = combinations_with_replacement(sorted_elements, num)
+
+    for i in combined_sorted_list:
+        print("".join(i))
